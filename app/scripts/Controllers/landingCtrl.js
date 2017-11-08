f71eceff18f4510d3ada65fe06f60942cac18606
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-11-08 15:02:08 -0800

    New Files

---
+++ app/scripts/Controllers/landingCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+     function LandingCtrl() {
+       this.heroTitle = "Electrify Your Party!";
+     }
+
+     angular
+         .module('blocJams')
+         .controller('LandingCtrl', LandingCtrl);
+ })();
+ 
