4fb17a3d632210075f1e7591df47c5949306ca6f
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-11-09 02:01:13 -0800

    completed-checkpoint-7

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -9,10 +9,10 @@
             year: '2017',
             albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-              { title: 'Roosevelt - Colours', duration: '259.80', audioUrl: 'assets/music/Colours.m4a' },
-              { title: 'Parcels - Overnight', duration: '219.00', audioUrl: 'assets/music/Overnight.m4a' },
-              { title: 'Breakbot - Why (feat.Ruckazoid)', duration: '322.04', audioUrl: 'assets/music/Why (feat. Ruckazoid).mp3' },
-              { title: 'Daft Punk feat.Todd Edwards - Fragments of Time',duration: '279.00', audioUrl: 'assets/music/Fragments of Time (feat. Todd Edwards).mp3' }
+              { title: 'Roosevelt - Colours', duration: '259.80', audioUrl: '/assets/music/Colours.m4a' },
+              { title: 'Parcels - Overnight', duration: '219.00', audioUrl: '/assets/music/Overnight.m4a' },
+              { title: 'Breakbot - Why (feat.Ruckazoid)', duration: '322.04', audioUrl: '/assets/music/Why (feat. Ruckazoid).mp3' },
+              { title: 'Daft Punk feat.Todd Edwards - Fragments of Time',duration: '279.00', audioUrl: '/assets/music/Fragments of Time (feat. Todd Edwards).mp3' }
             ]
          };
 
@@ -23,15 +23,15 @@
             year: '2017',
             albumArtUrl: '/assets/images/album_covers/20.png',
             songs: [
-              { title: 'Lucas Nord feat.Urban Cone - Let Us Stay Young', duration: '204.00', audioUrl: 'assets/music/Let Us Stay Young.m4a' },
-              { title: 'Two Door Cinema Club - Changing of The Seasons',duration: '223.00', audioUrl: 'assets/music/Changing of the Seasons.m4a' },
-              { title: 'Tahiti80 - Heartbeat', duration: '206.00', audioUrl: 'assets/music/Heartbeat.m4a' },
-              { title: 'Justice - New Lands', duration: '254.00', audioUrl: 'assets/music/New Lands.m4a' }
+              { title: 'Lucas Nord feat.Urban Cone - Let Us Stay Young', duration: '204.00', audioUrl: '/assets/music/Let Us Stay Young.m4a' },
+              { title: 'Two Door Cinema Club - Changing of The Seasons',duration: '223.00', audioUrl: '/assets/music/Changing of the Seasons.m4a' },
+              { title: 'Tahiti80 - Heartbeat', duration: '206.00', audioUrl: '/assets/music/Heartbeat.m4a' },
+              { title: 'Justice - New Lands', duration: '254.00', audioUrl: '/assets/music/New Lands.m4a' }
             ]
          };
 
          Fixtures.getAlbum = function() {
-           return albumElectronic;
+           return albumAlternative;
          };
          Fixtures.getCollection = function(numberOfAlbum) {
            arrayOfAlbum = [];
