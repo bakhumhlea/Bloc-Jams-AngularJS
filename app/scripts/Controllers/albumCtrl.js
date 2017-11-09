18ddf4c64dd4e812e553e2eae50f31f928ba3edd
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-11-09 02:04:31 -0800

    completed-checkpoint-7

--- app/scripts/Controllers/albumCtrl.js
+++ app/scripts/Controllers/albumCtrl.js
@@ -1,9 +1,10 @@
 (function() {
-  function albumCtrl(Fixtures) {
+  function albumCtrl(Fixtures, SongPlayer) {
     this.albumData = Fixtures.getAlbum();
+    this.songPlayer = SongPlayer;
   }
   angular
     .module('blocJams')
-    .controller('albumCtrl',['Fixtures',albumCtrl]);
+    .controller('albumCtrl',['Fixtures','SongPlayer', albumCtrl]);
 
   })();
